01364f44dc1f3160c49da87fe88dc7a0d3069174
Maritza Evian Valdez <maritzavaldez@mevaldez-mac.local>
2024-03-20 18:55:22 -0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import echoLogo from '/echo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={echoLogo} className="logo" alt="Echo Hub logo" />
+        </a>
+      </div>
+      <h1>Echo Hub</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
